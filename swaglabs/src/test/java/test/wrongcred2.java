14775b4f3423fcf9d178d116733621c3728c3115
sanjeve <121889022+sanjeve@users.noreply.github.com>
2024-09-19 09:30:27 +0530

    Testcases added

---
+++ swaglabs/src/test/java/test/wrongcred2.java
@@ -0,0 +1,52 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+
+@Test
+public class wrongcred2 {
+
+WebDriver driver = new ChromeDriver();
+	
+	@BeforeTest
+	public void beforeclass() {
+		driver.get("https://www.saucedemo.com/");
+	}
+	
+	@Test(priority=2)
+	public void testCorrectUserWrongPassword() {
+        // Wait setup
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        
+        // correct user wrong password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("123333");
+        driver.findElement(By.id("login-button")).click();
+        
+        // Wait and verify error message or incorrect URL
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")));
+        AssertJUnit.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/"));
+        
+        // Cleanup
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+	
+	
+	
+	
+	
+	
+}
